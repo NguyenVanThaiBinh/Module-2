73a2dd7580de84315d4cc93a3dd56c571a590825
Binh <nguyenvanthaibinh2210@gmail.com>
2021-03-30 15:26:13 +0700

    use var

---
+++ Module-2-Week-1/src/UseVar.java
@@ -0,0 +1,11 @@
+public class UseVar {
+    public static void main(String[] args) {
+        int i = 10;
+        float  f = 20.5f;
+        double d = 20.5;
+        boolean c = true;
+        char l ='a';
+        String str = "ha noi";
+        System.out.println("Var is: "+i+"\n"+f+"\n"+d+"\n"+c+"\n"+l+"\n"+str);
+    }
+}
