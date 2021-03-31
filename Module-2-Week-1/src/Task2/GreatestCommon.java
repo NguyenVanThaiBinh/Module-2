b6b49ea8bf00ea5bb27c0d3bdb3602f5747367f9
Binh <nguyenvanthaibinh2210@gmail.com>
2021-03-31 14:07:32 +0700

    Greatest common

---
+++ Module-2-Week-1/src/Task2/GreatestCommon.java
@@ -0,0 +1,28 @@
+package Task2;
+
+import java.util.Scanner;
+
+public class GreatestCommon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input number a:");
+        int a = scanner.nextInt();
+        System.out.println("Input number b:");
+        int b = scanner.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if(a==0 || b ==0){
+
+        }
+
+        while( a != b){
+            if(a>b) a = a-b;
+            else b= b -a;
+        }
+        System.out.println("Greatest common factor:"+ a);
+
+    }
+}
