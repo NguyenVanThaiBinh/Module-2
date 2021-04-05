c1841629f28342d0f6e35c7293aa3fa9d987a400
Binh <nguyenvanthaibinh2210@gmail.com>
2021-04-05 11:50:49 +0700

    fan

---
+++ Module-2-Week-1/src/task4/Fan.java
@@ -0,0 +1,68 @@
+package task4;
+
+public class Fan {
+    public Fan() {
+
+    }
+
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    private int speed = SLOW;
+    boolean on = true;
+    private double radius = 5;
+    private String color = "blue";
+
+    public String toString() {
+        if (this.on == true) {
+            return "Fan with speed: " + this.speed + "  radius: " + this.radius
+                    + " fan is on";
+        } else
+            return "Fan with speed: " + this.speed + "  radius: " + this.radius
+                    + " fan is off";
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(3);
+        fan1.setRadius(10);
+        fan1.setColor("Yello");
+        fan1.setOn(false);
+        System.out.println(fan1.toString());
+    }
+
+
+}
