4c5c48629829d3164432b2c4873ef8ccaa47eca2
Binh <nguyenvanthaibinh2210@gmail.com>
2021-04-05 15:00:54 +0700

    update

---
+++ Module-2-Week-1/src/task5/MyClass.java
@@ -0,0 +1,19 @@
+package task5;
+
+ public class MyClass {
+    public static void main(String[] args) {
+        Simple ob1 = new Simple();
+        
+    }
+
+}
+
+ class Simple{
+
+    private int a = 5;
+
+    private void msg() {
+        System.out.println("Hello java!");
+    }
+
+}
